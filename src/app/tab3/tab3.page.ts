27ff3fb64c2cd725802213f4c85b674dab473d92
Matthieu Charles <matth.charles@protonmail.com>
2022-07-07 12:08:01 +0200

    new build

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Trip, TripService } from '../trips.service';
+import { PhotoService, UserPhoto } from '../services/photo.service';
+import { ActionSheetController } from '@ionic/angular';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -11,10 +13,40 @@ export class Tab3Page implements OnInit {
 
   constructor(
     private tripService: TripService,
+    public photoService: PhotoService,
+    public actionSheetController: ActionSheetController
   ) {}
 
-  ngOnInit() {
+  async ngOnInit() {
     this.trips = this.tripService.getAll();
+    await this.photoService.loadSaved();
   }
+
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
+
+  public async showActionSheet(photo: UserPhoto, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+          }
+      }]
+    });
+    await actionSheet.present();
+  }
+
 
 }
